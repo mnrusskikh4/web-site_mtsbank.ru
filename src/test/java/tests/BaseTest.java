02dd8f6132f3172f156d5fc7d778ac7dc56f00db
Mikhail Russkikh <mnrusskikh4@gmail.com>
2025-01-12 23:32:39 +0300

    Тест сборки 4.1

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,23 +5,43 @@ import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public abstract class BaseTest {
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void globalSetup() {
         // 1. Автоматическая настройка и скачивание драйвера Chrome
         WebDriverManager.chromedriver().setup();
 
         // 2. Параметры Selenide
         Configuration.browser = "chrome";
-        Configuration.timeout = 10000;
-        Configuration.pageLoadStrategy = "eager";
-        // Configuration.headless = true; // - опционально
-        open("https://www.mtsbank.ru/"); // - чтобы инициализировать драйвер перед п.3
-        // 3. Очищаем историю, cookies, localStorage (чтобы тесты не мешали друг другу)
+        Configuration.browserSize = "1920x1080"; // Разрешение по умолчанию
+        Configuration.timeout = 10000; // Таймаут ожидания элементов
+        Configuration.pageLoadStrategy = "eager"; // Быстрая загрузка страницы
+        Configuration.headless = true; // Безголовый режим для CI/CD
+        Configuration.browserCapabilities.setCapability("goog:chromeOptions", new HashMap<>() {{
+            put("args", Arrays.asList(
+                    "--no-sandbox",
+                    "--disable-dev-shm-usage",
+                    "--disable-extensions",
+                    "--disable-gpu",
+                    "--disable-popup-blocking",
+                    "--disable-setuid-sandbox",
+                    "--remote-allow-origins=*"
+            ));
+        }});
+    }
+
+    @BeforeEach
+    void setUp() {
+        // 3. Открываем начальную страницу и очищаем браузерное состояние
+        open("https://www.mtsbank.ru/");
         clearBrowserCookies();
         clearBrowserLocalStorage();
     }
@@ -32,4 +52,3 @@ public abstract class BaseTest {
         Selenide.closeWebDriver();
     }
 }
-
